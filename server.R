9f1eb00a79b44588a44a6242f89734d5371d012c
Gabriel Nobrega <gabrielnobrega63@gmail.com>
2021-12-16 21:39:38 -0300

    comment

--- server.R
+++ server.R
@@ -1,8 +1,8 @@
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input,output) {
     ################### INPUT ####################
-    select_movie_stat <- eventReactive(input$go, {
+    select_movie_stat <- eventReactive(input$go,{
         
         movie_stat <- input$movie_stat
         switch (movie_stat,
@@ -14,12 +14,11 @@ server <- function(input, output) {
         twin <- input$true_date
         
         df_movie_stat <- master_df %>% 
-            select(Release.Date, movie_stat)
+            select(Release.Date,movie_stat)
 
         ## FALTA -> FILTRAR O DF POR DATA!!
 
         return(df_movie_stat)
-        # return(movie_stat)
     })
     
     output$time_date <- renderUI({
@@ -36,63 +35,73 @@ server <- function(input, output) {
         
         min_time <- min(df$Release.Date)
         max_time <- max(df$Release.Date)
-        dateRangeInput( "true_date", "Período de análise",
+        dateRangeInput( "true_date","Período de análise",
             end = max_time,
             start = min_time,
-            min  = min_time,
-            max  = max_time,
-            format = "dd/mm/yy",
-            separator = " - ",
+            min = min_time,
+            max = max_time,
+            format = "yyyy/mm/dd",
+            separator = "/",
             language='pt-BR'
         )
     })
     
-    output$time_date_comp <- renderUI({
+    # output$time_date_comp <- renderUI({
         
-        movie_stat <- input$movie_stat_comp
+    #     movie_stat <- input$movie_stat_comp
         
-        df <- master_df %>% 
-            filter(Index %in% movie_stat)
+    #     df <- master_df %>% 
+    #         filter(Index %in% movie_stat)
         
-        maxmin_time <- df %>% 
-            group_by(Index) %>% 
-            summarise(MD = min(Release.Date)) %>% 
-            .$MD %>% 
-            max()
+    #     maxmin_time <- df %>% 
+    #         group_by(Index) %>% 
+    #         summarise(MD = min(Release.Date)) %>% 
+    #         .$MD %>% 
+    #         max()
         
-        minmax_time <- df %>% 
-            group_by(Index) %>% 
-            summarise(MD = max(Release.Date)) %>% 
-            .$MD %>% 
-            min()
+    #     minmax_time <- df %>% 
+    #         group_by(Index) %>% 
+    #         summarise(MD = max(Release.Date)) %>% 
+    #         .$MD %>% 
+    #         min()
         
-        min_time <- maxmin_time
-        max_time <- minmax_time
+    #     min_time <- maxmin_time
+    #     max_time <- minmax_time
         
-        dateRangeInput("true_date_comp", "Período de análise",
-                       end = max_time,
-                       start = min_time,
-                       min    = min_time,
-                       max    = max_time,
-                       format = "dd/mm/yy",
-                       separator = " - ",
-                       language='pt-BR')
-    })
+    #     dateRangeInput("true_date_comp","Período de análise",
+    #                    end = max_time,
+    #                    start = min_time,
+    #                    min    = min_time,
+    #                    max    = max_time,
+    #                    format = "yyyy/mm/dd",
+    #                    separator = "/",
+    #                    language='pt-BR'
+    #     )
+    # })
     
     ################ OUTPUT #####################
     Info_DataTable <- eventReactive(input$go,{
         df <- select_movie_stat()
         
-        mean <- df %>% select(Close) %>% colMeans()
-        Media <- mean[[1]]
+        mean <- select(df,2) %>% colMeans()
+        Media <- mean[1]
+
+        # mod <- select(df,2) %>% //
+        # Moda <- mod[1]
+
+        # median <- select(df,2) %>% //
+        # Mediana <- median[1]
+
+        # desv <- <- select(df,2) %>% //
+        # Desvio <- desv[1]
         
-        Stock <- input$movie_stat
+        Atributo <- input$movie_stat
         
-        df_tb <-  data.frame(Stock, Media)
+        df_tb <- data.frame(Atributo,Media,Moda,Mediana,Desvio)
         
         df_tb <- as.data.frame(t(df_tb))
         
-        # tb  <- as_tibble(cbind(nms = names(df_tb), t(df_tb)))
+        # tb  <- as_tibble(cbind(nms = names(df_tb),t(df_tb)))
         # tb <- tb %>% 
         #     rename('Informações' = nms,
         #            'Valores' = V2)
@@ -100,34 +109,36 @@ server <- function(input, output) {
         return(df_tb)
     })
     
-    output$info <- renderDT({
-        Info_DataTable() %>%
-            as.data.frame() %>% 
-            DT::datatable(options=list(
-                language=list(
-                    url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json'
-                )
-            ))
-    })
+    # output$info <- renderDT({
+    #     Info_DataTable() %>%
+    #         as.data.frame() %>% 
+    #         DT::datatable(options=list(
+    #             language=list(
+    #                 url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json'
+    #             )
+    #         ))
+    # })
     
     output$sh <- renderPlot({
         # All the inputs
+
+        #Puxa uma tabela contendo só data e o atributo escolhido
         df <- select_movie_stat()
-        print(df)
         
-        # aux <- df$Close %>% na.omit() %>% as.numeric()
-        # aux1 <- min(aux)
-        # aux2 <- max(aux)
+        #a casa 1 (em r, 2) do array guarda o atributo a ser tratado
+        aux <- df[2]
+        aux1 <- min(aux)
+        aux2 <- max(aux)
         
         df$Release.Date <- ymd(df$Release.Date)
         a <- df %>% 
-            ggplot(aes(Release.Date, Close, group=1)) +
+            ggplot(aes(Release.Date,df[2],group=1)) +
             geom_path() +
             ylab('Preço da Ação em $') +
-            coord_cartesian(ylim = c(aux1, aux2)) +
+            coord_cartesian(ylim = c(aux1,aux2)) +
             theme_bw() +
-            scale_x_date(date_labels = "%Y-%m-%d")
-
+            scale_x_date(date_labels = "%m/%d/%Y")
+            
         a
     })
 }
